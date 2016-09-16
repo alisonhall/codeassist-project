b0f1f7fc84beb59bd9e71cc3e5a751d5a888b0d5
Alison Hall <alisonh0101@gmail.com>
2016-09-16 13:13:07 -0400

    Change to have multiple react sections: categories, languages, and examples

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Categories from './components/Categories.js';
+import Languages from './components/Languages.js';
+import Examples from './components/Examples.js';
+
+ReactDOM.render(
+  <Categories />,
+  document.getElementById('react-categories')
+);
+
+ReactDOM.render(
+  <Languages />,
+  document.getElementById('react-languages')
+);
+
+ReactDOM.render(
+  <Examples />,
+  document.getElementById('react-examples')
+);

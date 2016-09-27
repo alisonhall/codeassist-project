ab0628abfa1e091baa5d9ff369db7d3135d2a2e0
Alison Hall <alisonh0101@gmail.com>
2016-09-27 15:04:01 -0400

    Attempt to update the languages component to work the same way that the categories component works

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -5,60 +5,50 @@ import $ from 'jquery';
 
 
 class Languages extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      languages: []
-    };
-    this.eachLanguage = this.eachLanguage.bind(this);
-    // this.remove = this.remove.bind(this);
+	constructor(props) {
+		super(props);
+		this.state = {
+			allLanguages: [],
+			selectedLanguages: []
+		};
+		this.eachLanguage = this.eachLanguage.bind(this);
 
-    // if (typeof this.props.count !== 'number'){
-    //  return new Error('The count property must be a number');
-    // }
-  }
-  componentWillMount() {
-    var self = this;
-    $.getJSON('../../test-data.json', function(results){
-      $(results.languages).each(function(index, item){
-        self.add(item, index);
-        
-      });
-    });
-  }
-  add(item, index) {
-    var arr = this.state.languages;
-    arr.push({
-      id: index,
-      data: item
-    });
-    this.setState({languages: arr});
-  }
-  // eachCategory(category, i) {
-  //  return (
-  //    <Category key={category.id}
-  //      index={i}
-  //      onChange={this.update.bind(this)}
-  //      onRemove={this.remove}
-  //    >{category.category}</Category>
-  //  );
-  // }
-  eachLanguage(language, i) {
-    return (
-      <li>{language.data.fullName}</li>
-    );
-  }
-  render() {
-    return (
-      <div className='languages'>
-        <ul>
-          {this.state.languages.map(this.eachLanguage)}
-          <li><em>End of language list</em></li>
-        </ul>
-      </div>
+		var self = this;
+		$.getJSON('../../test-data.json', function(results){
+			$.each(results.languages, function(el, item) {
+				// console.log(el, item);
+				self.add(item, el);
+			});
+		});
+	}
 
-    );
-  }
+	add(item, el) {
+		console.log(item, el);
+		var allLanguages = this.state.allLanguages;
+		allLanguages[el] = item;
+		this.setState({allLanguages: allLanguages});
+		console.log(this.state);
+	}
+
+	eachLanguage(language, i) {
+		// console.log(language, i);
+		return (
+			<li key={i}>{language.name}</li>
+		);
+	}
+
+	render() {
+
+		return (
+			<div className='languages'>
+				<ul>
+					{this.state.allLanguages.map(this.eachLanguage)}
+					<li><em>End of language list</em></li>
+				</ul>
+			</div>
+
+		);
+	}
 }
 
 // Languages.propTypes = {

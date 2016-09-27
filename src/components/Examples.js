294b9a6e49a027abc7ea929a5697cc66b7dbf681
Alison Hall <alisonh0101@gmail.com>
2016-09-27 15:03:23 -0400

    Update the examples component to work the same way as the updated categories component

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -5,60 +5,44 @@ import $ from 'jquery';
 
 
 class Examples extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      examples: []
-    };
-    this.eachExample = this.eachExample.bind(this);
-    // this.remove = this.remove.bind(this);
+	constructor(props) {
+		super(props);
+		this.state = {
+			allExamples: []
+		};
+		this.eachExample = this.eachExample.bind(this);
 
-    // if (typeof this.props.count !== 'number'){
-    //  return new Error('The count property must be a number');
-    // }
-  }
-  componentWillMount() {
-    var self = this;
-    $.getJSON('../../test-data.json', function(results){
-      $(results.examples).each(function(index, item){
-        self.add(item, index);
-        
-      });
-    });
-  }
-  add(item, index) {
-    var arr = this.state.examples;
-    arr.push({
-      id: index,
-      data: item
-    });
-    this.setState({examples: arr});
-  }
-  // eachCategory(category, i) {
-  //  return (
-  //    <Category key={category.id}
-  //      index={i}
-  //      onChange={this.update.bind(this)}
-  //      onRemove={this.remove}
-  //    >{category.category}</Category>
-  //  );
-  // }
-  eachExample(example, i) {
-    return (
-      <li>{example.data.description}</li>
-    );
-  }
-  render() {
-    return (
-      <div className='examples'>
-        <ul>
-          {this.state.examples.map(this.eachExample)}
-          <li><em>End of example list</em></li>
-        </ul>
-      </div>
+		var self = this;
+		$.getJSON('../../test-data.json', function(results){
+			$.each(results.examples, function(index, item) {
+				self.add(item, index);
+			});
+		});
+	}
 
-    );
-  }
+	add(item, index) {
+		var allExamples = this.state.allExamples;
+		allExamples[index] = item;
+		this.setState({allExamples: allExamples});
+	}
+
+	eachExample(example, i) {
+		return (
+			<li key={i}>{example.description}</li>
+		);
+	}
+
+	render() {
+		return (
+			<div className='examples'>
+				<ul>
+					{this.state.allExamples.map(this.eachExample)}
+					<li><em>End of examples list</em></li>
+				</ul>
+			</div>
+
+		);
+	}
 }
 
 // Categories.propTypes = {
